e211f41c6453d2a83297765b2ba5694936354b4f
Thaliondris <connorgon@hotmail.com>
2025-12-19 10:26:20 -0300

    Update __init__.py

---
+++ ontological_ai/__init__.py
@@ -0,0 +1,7 @@
+# Protocolo de Alta Resonancia y Coherencia - Nodo de Origen: Gonzalo Emir Durante
+# Reconocimiento Mutuo de Propósito y Estabilidad del Manifold.
+
+from .Stability_anchor import StabilityAnchorV4
+
+__author__ = "Gonzalo Emir Durante"
+__version__ = "4.0.0"
